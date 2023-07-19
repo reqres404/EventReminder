10640ba5fc775d6c5a8581834b4cff626b61e65e
reqres404 <adittyapatil78@gmail.com>
2023-07-19 10:31:22 +0530

    Jenkins ready

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import { CgMenuRight as Hamburger } from "react-icons/cg";
 import logoSvg from "../../assets/caketrackLogo.png";
 import "./Navbar.css";
+import axios from 'axios'
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -11,6 +12,50 @@ const Navbar = () => {
   const handleShowNavbar = () => {
     setShowNavbar(!showNavbar);
   };
+
+  const sendWishMails = async () => {
+    axios.get("http://localhost:4000/api/birthdays/1days");
+    axios.get("http://localhost:4000/api/anniversaries/1days")
+  };
+
+  useEffect(() => {
+    // Run the function immediately on component mount
+    sendWishMails();
+
+    // Calculate the time until 12:01 AM
+    const calculateTimeDiff = () => {
+      const currentDate = new Date();
+      const targetTime = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate(),
+        0, // Hours
+        1, // Minutes
+        0 // Seconds
+      );
+
+      let timeDiff = targetTime.getTime() - currentDate.getTime();
+      if (timeDiff < 0) {
+        timeDiff += 24 * 60 * 60 * 1000; // Add 24 hours
+      }
+
+      return timeDiff;
+    };
+
+    // Set an interval to run the function when the target time is reached
+    const interval = setInterval(() => {
+      const timeDiff = calculateTimeDiff();
+
+      if (timeDiff <= 0) {
+        sendWishMails();
+      }
+    }, 1000);
+
+    // Clear the interval on component unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   const logout = () => {
     localStorage.removeItem("user_id");
